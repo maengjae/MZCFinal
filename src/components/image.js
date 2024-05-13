6efac6ae1113a1c376f25e33a45288a1e866d082
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-14 08:03:28 +0900

    image testing

--- src/components/image.js
+++ src/components/image.js
@@ -1,7 +1,6 @@
-import { Box } from '@mui/material';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import S3FileUpload from 'react-s3';
- 
+
 const config = {
     bucketName: 'mjy-static-deploy',
     dirName: 'photos', /* optional */
@@ -10,30 +9,34 @@ const config = {
     secretAccessKey: process.env.Secret_access_key,
 }
 
-class Image extends Component {
-    constructor(){
-        super();
-    }
-    upload(e){
-        console.log(e.target.files[0]);
-        S3FileUpload.upload(e.target.files[0],config)
-        .then((data)=>{
-            console.log(data.location);
-        })
-        .catch((err)=>{
-            alert(err);
-        })
-    }
-    render(){
-        return (
-            <Box>
-                <h3>
-                    aws s3 upload
-                </h3>
-                <input type="file" onChange={this.upload}/> 
-            </Box>
-        )
-    }
+const ImageUploader = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const upload = () => {
+        if (selectedFile) {
+            S3FileUpload.upload(selectedFile, config)
+                .then((data) => {
+                    console.log(data.location);
+                })
+                .catch((err) => {
+                    alert(err);
+                });
+        } else {
+            alert('Please select a file first.');
+        }
+    };
+
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    return (
+        <div>
+            <h3>Upload Image to S3</h3>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={upload}>Upload</button>
+        </div>
+    );
 }
 
-export default Image
+export default ImageUploader;
