8b02c0c117f2d19e883d3355767ab20f74bb3c16
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-14 07:14:48 +0900

    image testing

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,4 @@
-import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import S3FileUpload from 'react-s3';
  
 const config = {
@@ -10,33 +9,28 @@ const config = {
     secretAccessKey: process.env.Secret_access_key,
 }
 
-export default function Image() {
-    const [selectedFile, setSelectedFile] = useState(null);
-
-    const upload = () => {
-        if (selectedFile) {
-            S3FileUpload.upload(selectedFile, config)
-                .then((data) => {
-                    console.log(data.location);
-                })
-                .catch((err) => {
-                    alert(err);
-                });
-        } else {
-            alert('Please select a file first.');
-        }
-    };
-
-    const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0]);
-    };
-
-    return (
-        <Box>
-            <h3>aws s3 upload</h3>
-            <input type="file" style={{ display: 'fileimage' }} onChange={handleFileChange} ref={(fileInput) => (this.fileInput = fileInput)} />
-            <button onClick={() => this.fileInput.click()}>Select File</button>
-            <button onClick={upload}>Upload</button>
-        </Box>
-    );
+export class image extends Component {
+    constructor(){
+        super();
+    }
+    upload(e){
+        console.log(e.target.files[0]);
+        S3FileUpload.upload(e.target.files[0],config)
+        .then((data)=>{
+            console.log(data.location);
+        })
+        .catch((err)=>{
+            alert(err);
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h3>
+                    aws s3 upload
+                </h3>
+                <input type="file" onChange={this.upload}/> 
+            </div>
+        )
+    }
 }
