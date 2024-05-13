51cf4ada82aaae46b960b1678323ad80447e5814
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-13 23:00:49 +0900

    image testing

---
+++ src/components/image.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import S3FileUpload from 'react-s3';
+ 
+const config = {
+    bucketName: 'mjy-static-deploy',
+    dirName: 'photos', /* optional */
+    region: 'us-east-2',
+    accessKeyId: process.env.Access_key_ID,
+    secretAccessKey: process.env.Secret_access_key,
+}
+
+export default function Image() {
+    const upload = (e) => {
+        console.log(e.target.files[0]);
+        S3FileUpload.upload(e.target.files[0], config)
+        .then((data) => {
+            console.log(data.location);
+        })
+        .catch((err) => {
+            alert(err);
+        })
+    }
+
+    return (
+        <div>
+            <h3>aws s3 upload</h3>
+            <input type="file" onChange={upload}/>
+        </div>
+    );
+}
