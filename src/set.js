9d34072e32c4145dc17f816b035eb2e8606bb976
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-14 13:16:37 +0900

    image test

--- src/set.js
+++ src/set.js
@@ -6,6 +6,6 @@ export const config = {
     bucketName: 'mjy-static-deploy',
     dirName: 'images', /* optional */
     region: 'us-east-2',
-    // accessKeyId: process.env.Access_key_ID,
-    // secretAccessKey: process.env.Secret_access_key,
+    accessKeyId: process.env.Access_key_ID,
+    secretAccessKey: process.env.Secret_access_key,
 }
