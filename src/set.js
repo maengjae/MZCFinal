912f6a4acc28f04e9d777deb756620b76778c003
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-17 18:17:56 +0900

    add

--- src/set.js
+++ src/set.js
@@ -2,6 +2,6 @@ export const config = {
     bucketName: 'mjy-static-deploy',
     dirName: 'images', /* optional */
     region: 'us-east-2',
-    accessKeyId: process.env.Access_key_ID,
-    secretAccessKey: process.env.Secret_access_key,
+    accessKeyId: process.env.REACT_APP_ID,
+    secretAccessKey: process.env.REACT_APP_SECRET
 }
