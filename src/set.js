4bfb16a3c0e1b884409513b9ab3a7fdf9d870c6a
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-17 20:23:31 +0900

    add

--- src/set.js
+++ src/set.js
@@ -4,5 +4,5 @@ export const config = {
     region: 'us-east-2',
     accessKeyId: process.env.REACT_APP_ID,
     secretAccessKey: process.env.REACT_APP_SECRET,
-    s3Url: 'http://mjy-static-deploy.s3.us-east-2.amazonaws.com/'
+    s3Url: 'https://mjy-static-deploy.s3.us-east-2.amazonaws.com/'
 }
