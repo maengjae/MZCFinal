489e760862c78518e5fd9f61908ac4f37069806a
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-17 19:57:23 +0900

    http

--- src/set.js
+++ src/set.js
@@ -3,5 +3,6 @@ export const config = {
     dirName: 'images', /* optional */
     region: 'us-east-2',
     accessKeyId: process.env.REACT_APP_ID,
-    secretAccessKey: process.env.REACT_APP_SECRET
+    secretAccessKey: process.env.REACT_APP_SECRET,
+    s3Url: 'https:/mjy-static-deploy.s3.us-east-2.amazon.com/'
 }
