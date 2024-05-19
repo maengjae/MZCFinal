e13a492aa27afaf95b44c3bc3df75ec9c16e141f
maengjae <112834736+maengjae@users.noreply.github.com>
2024-05-19 23:19:57 +0900

    pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Appbar from "./components/Appbar"
-import Cosmetic from "./components/Cosmetics"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Appbar from "./components/Appbar";
+import Cosmetic from "./components/Cosmetics";
 import CosmeticDetail from './components/CosmeticDetail';
 
 function App() {
   return (
-    <div className="App">
-    <Appbar/>
-    <Cosmetic/>
-    <Route path="cosmetic/:id" element={<CosmeticDetail />} />
-    </div>
+    
+    <Router>
+      <div className="App">
+        <Appbar />
+        <Routes>
+          <Route path="/" element={<Cosmetic />} />
+          <Route path="/cosmetic/:id" element={<CosmeticDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
