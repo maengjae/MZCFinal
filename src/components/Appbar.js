77df1b5fc11955cfab4f4ffff1de483aa6ea9d65
maengjae <112834736+maengjae@users.noreply.github.com>
2024-06-05 09:31:34 +0900

    check

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -23,7 +23,7 @@ export default function Appbar({ username, handleLogout }) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MJY online cosmetic shop complete
+            MJY online cosmetic shop!!!!!!
           </Typography>
           {username ? (
             <>
